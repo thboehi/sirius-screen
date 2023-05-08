23207c7054ed1a2d42e9adfb7bab562e44309373
Thoma <thomaboehi@gmail.com>
2023-05-08 17:42:25 +0200

    Changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const drinksContainer = document.getElementById("drinks-container");
 const foodsContainer = document.getElementById("food-container");
+const popcornsContainer = document.getElementById("popcorns-container");
 let consommables = [];
 let totalConsommables = 0;
 
@@ -75,10 +76,38 @@ function addFoodToPage() {
       } else {
       }
     }
+}
+
+  function addPopCornToPage() {
+    for (let i = 0; i < consommables.length; i++) {
+      if (consommables[i].type === "popcorn"){
+          const popcornContainer = document.createElement("div");
+          popcornContainer.classList.add("type-container");
+  
+          const image = document.createElement("img");
+          image.classList.add("img");
+          image.src = "./img/" + consommables[i].img;
+          image.alt = consommables[i].name;
+  
+          const title = document.createElement("h3");
+          title.textContent = consommables[i].name;
+          title.classList.add("title");
+  
+          const price = document.createElement("p");
+          price.textContent = consommables[i].price;
+          price.classList.add("price");
+  
+          popcornContainer.appendChild(image);
+          popcornContainer.appendChild(title);
+          popcornContainer.appendChild(price);
+          popcornsContainer.appendChild(popcornContainer);
+      } else {
+      }
+    }
   }
 
 getConsommables().then(() => {
     addDrinksToPage();
     addFoodToPage();
-  
+    addPopCornToPage()
 });
